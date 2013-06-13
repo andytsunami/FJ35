7f3030ce4a9d3196d2e15401e7c17fb6b148dc7c
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-06-13 16:25:20 -0300

    Capitulo 04 - Atividade 06 - Criando classe Movimentação e seu respectivo ENUM

---
+++ src/br/com/caelum/financas/modelo/Movimentacao.java
@@ -0,0 +1,80 @@
+package br.com.caelum.financas.modelo;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class Movimentacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private BigDecimal valor;
+
+	@Temporal(TemporalType.DATE)
+	private Calendar data;
+	private String descricao;
+
+	@ManyToOne
+	private Conta conta;
+
+	@Enumerated(EnumType.STRING)
+	private TipoMovimentacao tipoMovimentacao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+	public TipoMovimentacao getTipoMovimentacao() {
+		return tipoMovimentacao;
+	}
+
+	public void setTipoMovimentacao(TipoMovimentacao tipoMovimentacao) {
+		this.tipoMovimentacao = tipoMovimentacao;
+	}
+
+}
