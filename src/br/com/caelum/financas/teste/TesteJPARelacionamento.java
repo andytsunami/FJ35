e748700b50b20fcefed30cb45b4122aebae9940b
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-06-13 16:56:28 -0300

    Capitulo 04 - Atividade 08 - Testando relacionamento entre movimentacao e conta

--- src/br/com/caelum/financas/teste/TesteJPARelacionamento.java
+++ src/br/com/caelum/financas/teste/TesteJPARelacionamento.java
@@ -1,11 +1,16 @@
 package br.com.caelum.financas.teste;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
 
 import br.com.caelum.financas.jpa.util.JPAUtil;
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
 
-public class TesteEstadosJPA {
+public class TesteJPARelacionamento {
 
 	public static void main(String[] args) {
 
@@ -15,11 +20,19 @@ public class TesteEstadosJPA {
 		conta.setAgencia("043");
 		conta.setNumero("54321");
 
+		Movimentacao movimentacao = new Movimentacao();
+		movimentacao.setData(Calendar.getInstance());
+		movimentacao.setDescricao("Conta de luz");
+		movimentacao.setTipoMovimentacao(TipoMovimentacao.SAIDA);
+		movimentacao.setValor(new BigDecimal(123.9));
+		movimentacao.setConta(conta);
+
 		EntityManager em = new JPAUtil().getEntityManager();
 
 		em.getTransaction().begin();
 
 		em.persist(conta);
+		em.persist(movimentacao);
 
 		em.getTransaction().commit();
 		em.close();
