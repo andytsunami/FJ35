3ce00af992d878e76329d56ec8b8fc93f5297aa5
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-06-17 17:57:20 -0300

    Capitulo 07 - Atividade 10 - Encapsulando as consultas com o padrão DAO

---
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -0,0 +1,31 @@
+package br.com.caelum.financas.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+
+public class MovimentacaoDao {
+
+	private EntityManager em;
+
+	public MovimentacaoDao(EntityManager em) {
+		this.em = em;
+	}
+
+	public Double mediaDaConta(Conta conta) {
+
+		TypedQuery<Double> query = em
+				.createQuery(
+						"select avg(m.valor) from Movimentacao m where m.conta = :conta and m.tipoMovimentacao = :tipo",
+						Double.class);
+
+		query.setParameter("conta", conta);
+		query.setParameter("tipo", TipoMovimentacao.SAIDA);
+
+		return query.getSingleResult();
+
+	}
+
+}
---
+++ src/br/com/caelum/financas/teste/TestaConsultaComDAO.java
@@ -0,0 +1,24 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.dao.MovimentacaoDao;
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TestaConsultaComDAO {
+
+	public static void main(String[] args) {
+
+		EntityManager em = new JPAUtil().getEntityManager();
+
+		MovimentacaoDao dao = new MovimentacaoDao(em);
+
+		Conta conta = new Conta();
+		conta.setId(1);
+
+		System.out.println(dao.mediaDaConta(conta));
+
+	}
+
+}
